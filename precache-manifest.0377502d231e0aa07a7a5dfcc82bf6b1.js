1672e6bcdb2183a102fce51b2922bbd202d28943
nicolasariza <nicolas_ariza@outlook.com>
2020-04-05 11:18:23 -0500

    Updates

---
+++ precache-manifest.0377502d231e0aa07a7a5dfcc82bf6b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627ed5f1d9b32e26c7da2c7600091173",
+    "url": "/musica/index.html"
+  },
+  {
+    "revision": "b74aecf18a2411b5b0b9",
+    "url": "/musica/static/css/main.aa4769a3.chunk.css"
+  },
+  {
+    "revision": "48364b632d2c7af16ad0",
+    "url": "/musica/static/js/2.82fd59f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/musica/static/js/2.82fd59f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b74aecf18a2411b5b0b9",
+    "url": "/musica/static/js/main.d3db6ff6.chunk.js"
+  },
+  {
+    "revision": "d5ebd35c041316f43d87",
+    "url": "/musica/static/js/runtime-main.b694a9da.js"
+  }
+]);
